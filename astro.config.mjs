68f6240f8ab1b311ca9f9cba6ae4a9dfa8c11ea0
andresMZ-ec <andresmz.contact@gmail.com>
2025-08-11 21:20:33 -0500

    deploy: Configuración final para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://andresmz-ec.github.io/net-telerik-prueba/',
+	base: '/net-telerik-prueba/',
 	integrations: [
 		starlight({
 			title: 'Bilbo S.A',
